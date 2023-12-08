961e0a01d727d5c2cec50fecf5c9c8f5559b8543
SamyAlaa10 <samyalaa638@gmail.com>
2023-12-08 11:57:47 +0200

    samy alaa 8-12-2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'طريقك';
-
 }
